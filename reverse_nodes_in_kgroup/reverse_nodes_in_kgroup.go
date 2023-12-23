0cede63d551ab6f31372db120bb340804276acdc
foroozf001 <faraz.foroozan@nordcloud.com>
2023-12-23 21:14:04 +0100

    added GetNode methods iteratively and recursively
    Signed-off-by: foroozf001 <faraz.foroozan@nordcloud.com>

--- reverse_nodes_in_kgroup/reverse_nodes_in_kgroup.go
+++ reverse_nodes_in_kgroup/reverse_nodes_in_kgroup.go
@@ -1,6 +1,15 @@
 package main
 
-func main() {}
+func main() {
+	l1e := &ListNode{5, nil}
+	l1d := &ListNode{4, l1e}
+	l1c := &ListNode{3, l1d}
+	l1b := &ListNode{2, l1c}
+	l1a := &ListNode{1, l1b}
+
+	l1a.GetNodeIteratively(5).Print()
+	l1a.GetNodeRecursively(5).Print()
+}
 
 type ListNode struct {
 	Val  int
@@ -12,6 +21,21 @@ func (l *ListNode) Print() {
 	if l.Next != nil {
 		l.Next.Print()
 	}
+}
+
+func (l *ListNode) GetNodeRecursively(k int) *ListNode {
+	if k > 0 && l.Next != nil {
+		return l.Next.GetNodeRecursively(k - 1)
+	}
+	return l
+}
+
+func (l *ListNode) GetNodeIteratively(k int) *ListNode {
+	var j int
+	for ; j < k && l.Next != nil; j++ {
+		l = l.Next
+	}
+	return l
 }
 
 func reverseKGroup(head *ListNode, k int) *ListNode {
